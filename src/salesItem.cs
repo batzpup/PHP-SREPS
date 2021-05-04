86b752101d4255f51d5d805cc48e1a91238cedc1
illuminati_confirmed <nathanj428@gmail.com>
2021-05-05 09:19:56 +1000

    added salesItem class [Nathan]

---
+++ src/salesItem.cs
@@ -0,0 +1,22 @@
+public class SalesItem {
+    private string id;
+    private int quantity;
+    public void SalesItem(string id, int quantity) {
+        id = id;
+        quantity = quantity;
+    }
+
+    public string getId() {
+        return id;
+    }
+    public void setId(string id) {
+        id = id;
+    }
+
+    public int getQuantity() {
+        return quantity;
+    }
+    public void setQuantity() {
+        quantity=quantity;
+    }
+}
